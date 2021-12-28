cafea9ad68c2bd42788e7b6ee2d395046acb3770
Mohamed Salah <maxbayne@gmail.com>
2021-12-28 23:11:38 +0200

    Add project files.

---
+++ UnitTesting/UserManager.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTesting
+{
+    public class UserManager
+    {
+        public bool Login(string userName,string password)
+        {
+            if (userName == "admin" && password == "123")
+                return true;
+            else
+                return false;
+            
+        }
+    }
+}
