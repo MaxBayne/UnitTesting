c7f76e721de7099794544b26efd1b87c861666da
Mohamed Salah <maxbayne@gmail.com>
2021-12-28 23:11:38 +0200

    Add project files.

---
+++ UnitTesting/UserManager.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTesting
+{
+    public interface IUserManager
+    {
+        bool Login(string userName,string password);
+    }
+
+    public class UserManager : IUserManager
+    {
+        public bool Login(string userName,string password)
+        {
+            if (userName == "admin" && password == "123")
+                return true;
+            else
+                return false;
+            
+        }
+    }
+}
---
+++ UnitTesting/UserManagerTests.cs
@@ -0,0 +1,53 @@
+using System;
+using NSubstitute;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class UserManagerTests
+    {
+        //Name Methods For Tests using this Conventions
+        //UnitOfWork_ScenarioForTest_ExpectedResults
+        //UserManager_LoginAdmin_ReturnTrue
+
+        private IUserManager _userManager;
+
+
+        public UserManagerTests()
+        {
+            _userManager = Substitute.For<IUserManager>();
+        }
+
+        [Fact]
+        public void Security_LoginAdmin_ReturnTrue()
+        {
+            //Arrange (Mean Set Variables and take instances from objects that test need)
+            var userManager = new UserManager();
+
+
+            //Act (Mean Call Function to Test depend on Arrange step)
+            var result = userManager.Login("admin", "123");
+
+            //Assert (Mean Check if result equal to expected result to say this test is success or fail)
+            Assert.True(result);
+
+        }
+
+        [Theory]
+        [InlineData("user1","pass1")]
+        [InlineData("user2", "pass2")]
+        [InlineData("user3", "pass3")]
+        public void Security_LoginUser_ReturnTrue(string userName,string password)
+        {
+            //Arrange (Mean Set Variables and take instances from objects that test need)
+            var userManager = new UserManager();
+
+
+            //Act (Mean Call Function to Test depend on Arrange step)
+            var result = userManager.Login(userName, password);
+
+            //Assert (Mean Check if result equal to expected result to say this test is success or fail)
+            Assert.True(result);
+        }
+    }
+}
